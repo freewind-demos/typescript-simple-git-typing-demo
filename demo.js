c063d8adc93aa7988095c8012155c7b9289f7a32
freewind <nowindlee@gmail.com>
2018-09-08 22:57:52 +0800

    improve code

--- demo.js
+++ demo.js
@@ -2,5 +2,6 @@ const workingDirPath = './local-repo'
 const simpleGit = require('simple-git')(workingDirPath)
 
 simpleGit.clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', 'demo-project')
-
-console.log('cloned to: local-repo/demo-project')
+    .exec(function () {
+        console.log('cloned to: local-repo/demo-project')
+    })
